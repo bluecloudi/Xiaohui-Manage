f230afbe190e27f797f8700f4968d1b72ea98a21
bluecloudi <1847315825@qq.com>
2019-11-04 21:19:56 +0800

    npm install

--- gulpfile.js/index.js
+++ gulpfile.js/index.js
@@ -1,5 +1,4 @@
 const{clean}=require('./clean');
 const{compilestylus}=require('./stylus');
 const{series}=require('gulp');
-const{test}=require('./test');
-exports.default= series(clean,compilestylus,test);
+exports.default= series(clean,compilestylus);
--- gulpfile.js/test.js
+++
@@ -1,7 +0,0 @@
-const{electron}=require('electron');
-const{gulp}=require('gulp');
-function test(cb){
-    electron.start();
-    cb();
-}
-exports.test=test;
